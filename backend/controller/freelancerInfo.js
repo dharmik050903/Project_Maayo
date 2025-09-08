26447b132fc215f4da1c1c0fdc2f629fa402e10e
Dharmik <Dharmiksuthar0509@gmai.com>
2025-09-08 11:40:57 +0530

    List freelancer

--- backend/controller/freelancerInfo.js
+++ backend/controller/freelancerInfo.js
@@ -3,6 +3,31 @@ import PersonMaster from "../schema/PersonMaster.js";
 
 
 export default class FreelancerInfo {
+    async listfreelancer(req,res){
+        try {
+            if(req.body.id && req.body.user_role === "freelancer"){
+                const freelancer = await freelancerInfo.findById(req.body.id)
+                if(!freelancer){
+                    res.status(404).json({ message: "Freelancer info not found" });
+                }
+                if(freelancer){
+                    res.status(200).json({ message: "Freelancer info retrieved successfully", data: freelancer });
+                }
+            }
+            if(req.body.user_role === "freelancer" && req.body.name){
+                const freelancers = await freelancerInfo.find({name: { $regex: req.body.name, $options: 'i' }})
+                if(freelancers.length === 0){
+                    res.status(404).json({ message: "No freelancers found" });
+                }
+                if(freelancers.length > 0){
+                    res.status(200).json({ message: "Freelancers retrieved successfully", data: freelancers });
+                }
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Failed to retrieve freelancer info", error: error.message });
+            console.error("Error retrieving freelancer info:", error);
+        }
+    }
     async createFreelancerInfo(req, res) {
         try {
             if (req.headers.user_role !== 'freelancer') {
